10fe28d992dba3316db7c96686e72bd654e2c82d
Bahrom0311 <162051565+Bahrom0311@users.noreply.github.com>
2026-01-29 23:55:08 +0500

    docker

--- app/core/config.py
+++ app/core/config.py
@@ -86,7 +86,8 @@ class Settings(BaseSettings):
     
     model_config = SettingsConfigDict(
         env_file=".env",
-        env_file_encoding="utf-8"
+        env_file_encoding="utf-8",
+        extra="ignore",
     )
 
 settings = Settings()
